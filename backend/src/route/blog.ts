8d239ddb3f7ff68d36e2a96126e0716c5e708ee5
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-03-07 16:36:39 +0530

    completed backend

--- backend/src/route/blog.ts
+++ backend/src/route/blog.ts
@@ -2,55 +2,55 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { verify } from 'hono/jwt'
-import { createBlogInput , updateBlogInput} from "@rakeshkanneeswaran/mediumblog-common/dist/zod";
+import { createBlogInput, updateBlogInput } from "@rakeshkanneeswaran/mediumblog-common/dist/zod";
 
 export const blogRouter = new Hono<{
     Bindings: {
         DATABASE_URL: string
         JWT_KEY: string
     },
-    Variables:{
-        userId : string
+    Variables: {
+        userId: string
     }
 }>();
 
 
-blogRouter.use("/*",async (c ,next) => {
+blogRouter.use("/*", async (c, next) => {
     const authHeader = c.req.header("authorization") || " ";
 
     try {
-        const user = await verify(authHeader,c.env.JWT_KEY);
+        const user = await verify(authHeader, c.env.JWT_KEY);
         if (user) {
-            c.set("userId" , user.id)
+            c.set("userId", user.id)
             await next();
         } else {
             c.status(403);
             return c.json({
-                "message" : "you are not logged In "
+                "message": "you are not logged In "
             })
         }
         next();
-        
+
     } catch (error) {
         c.status(400)
 
         return c.json({
-            error : "not authorised"
+            error: "not authorised"
         })
-        
+
     }
-   
+
 })
 
 
 blogRouter.post('/', async (c) => {
     const body = await c.req.json();
-    const {success} = createBlogInput.safeParse(body);
+    const { success } = createBlogInput.safeParse(body);
 
     if (!success) {
         c.status(411);
         return c.json({
-            error : "inputs not correct"
+            error: "inputs not correct"
         })
     }
     const authorId = c.get("userId")
@@ -72,11 +72,11 @@ blogRouter.post('/', async (c) => {
 
 blogRouter.put('/', async (c) => {
     const body = await c.req.json();
-    const {success} = updateBlogInput.safeParse(body);
+    const { success } = updateBlogInput.safeParse(body);
     if (!success) {
         c.status(411);
         return c.json({
-            error : "inputs not correct"
+            error: "inputs not correct"
         })
     }
     const prisma = new PrismaClient({
@@ -100,15 +100,15 @@ blogRouter.get('/bulk', async (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-     try {
+    try {
         const blog = await prisma.blog.findMany({
-            select : {
-                content : true,
-                title : true,
-                id : true,
-                author : {
-                    select : {
-                        name : true
+            select: {
+                content: true,
+                title: true,
+                id: true,
+                author: {
+                    select: {
+                        name: true
                     }
                 }
             }
@@ -117,18 +117,18 @@ blogRouter.get('/bulk', async (c) => {
             blog
 
         })
-     } catch (error) {
+    } catch (error) {
         c.status(411);
         return c.json({
-            error : "error occoured"
+            error: "error occoured"
         })
-     }
+    }
 })
 
 
 blogRouter.get('/:id', async (c) => {
 
-    const id =  c.req.param("id");
+    const id = c.req.param("id");
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
@@ -138,12 +138,23 @@ blogRouter.get('/:id', async (c) => {
             where: {
                 id: Number(id)
             }
+            ,
+            select: {
+                title: true,
+                content: true,
+                id: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
         })
         return c.json({
             blog
         })
 
-    } 
+    }
     catch (error) {
         c.status(411);
         return c.json({
